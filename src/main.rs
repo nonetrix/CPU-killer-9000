50905e5bf7114e70b171a24f045416839e9f0360
nonetrix <45698918+nonetrix@users.noreply.github.com>
2020-12-11 11:30:43 -0600

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::thread;
+use rand::Rng;
+fn main() {
+    loop {
+        thread::spawn(move || {
+           loop {
+                let correct_guess: i32 = rand::thread_rng().gen_range(0, 2147483647);
+                let guess: i32 = rand::thread_rng().gen_range(0, 2147483647);
+                if guess == correct_guess {
+                    println!("The correct number is: {}", guess)
+                }
+           }
+
+        });
+    }
+}
